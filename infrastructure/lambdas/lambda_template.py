0240ea493247769893020c449980b00f4439f0f9
Andrew G. Saushkin <asaushkin@gmail.com>
2022-07-06 18:10:53 +0400

    Fix json structure

--- infrastructure/lambdas/lambda_template.py
+++ infrastructure/lambdas/lambda_template.py
@@ -2,6 +2,12 @@ import json
 import os
 import utils
 
+# Three steps to create a new lambda function:
+# 1. Create a new python function based on this code (remember a new file name)
+# 2. Create a part in terraform lambda.tf file
+# 3. Add API gateway endpoint and link API integration with terraform code
+
+
 def handler(event, context):
     utils.log().info('Got event data: %s', json.dumps(event))
     utils.log().debug('Environ: %s', os.environ)
